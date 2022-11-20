309da3dbf580af10ea81848dc6fcc17686fcee06
ajkn1904 <ajknishat@gmail.com>
2022-11-21 03:02:21 +0600

    doctors portal server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async(req, res) => {
+    res.send('Doctors Portal is running');
+});
+
+app.listen(port, () => console.log(`Doctors portal is running on ${port}`))
